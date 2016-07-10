f45c76f6bae1f7bf1e87b1a3ea989ee4e142f807
jaehyunbaek <happyroter@gmail.com>
2016-07-10 20:52:15 +0900

    fix NUMBERGAME

--- algospot/NUMBERGAME.cpp
+++ algospot/NUMBERGAME.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #define max(a, b) a>b?a:b
+#define min(a, b) a<b?a:b
 #define NA -999999999
 
 
@@ -8,20 +9,28 @@ int cache[51][51];
 
 int dp(int* data, int left, int right)
 {
-	if (left > right)
-		return 0;
+	if (left == right)
+		return data[left];
+	else if (right - left == 1)
+	{
+		int more = max(data[left], data[right]);
+		int less = min(data[left], data[right]);
+		return more - less;
+	}
 
 	if (cache[left][right] != NA)
 		return cache[left][right];
 
-	cache[left][right] = max(data[left] - dp(data, left + 1, right), data[right] - dp(data, left, right - 1));
+	int val = max(data[left] - dp(data, left + 1, right), data[right] - dp(data, left, right - 1));
 
 	if (right - left >= 1)
 	{
-		cache[left][right] = max(-dp(data, left + 2, right), cache[left][right]);
-		cache[left][right] = max(cache[left][right], -dp(data, left, right - 2));
+		val = max(-dp(data, left + 2, right), val);
+		val = max(val, -dp(data, left, right - 2));
 	}
-	return cache[left][right];
+
+	cache[left][right] = val;
+	return val;
 }
 
 
